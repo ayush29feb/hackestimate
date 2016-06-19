93e665abb23909d761a7d48b5b3bcaeefbc5dee2
Ayush Saraf <ayush29f@uw.edu>
2016-06-18 23:52:18 -0700

    Basic Logic Setup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import './styles/main.scss';
-const component = require('./hello');
+import { finalhackers as logic } from './logic';
 
-document.body.appendChild(component());
+const mockapps = [
+  {
+    rank: 1,
+    name: 'Ayush',
+    airport: 'SEA',
+  }, {
+    rank: 2,
+    name: 'Cedric',
+    airport: 'LAX',
+  }, {
+    rank: 3,
+    name: 'Aishu',
+    airport: 'SFO',
+  },
+];
+
+console.log(logic(mockapps, 10000, 600));
---
+++ src/logic.js
@@ -0,0 +1,35 @@
+import _ from 'lodash';
+
+const LOCAL_HACKER_RATIO = 0.7;
+console.log('hi');
+// localApplications is a subset of totalApplications who are in the regional area
+function localHackers(localApplications, localHackersTotal) {
+  // Sort by rank and return the new object totalLocalHackers
+  localApplications.sort((a, b) => (a.rank > b.rank ? -1 : 1));
+  const finalLocalHackers = localApplications.slice(0, localHackersTotal - 1);
+  return finalLocalHackers;
+}
+
+function remoteHackers(remoteApplications, budget, totalHackers) {
+  // normalize this distribution:
+  // google a normalization function
+  // optimize on constraints
+  // return the list of accepted hackers
+  console.log(budget, totalHackers);
+}
+
+// Pre: Takes in a array of objects containing all the applications,
+//      Also takes in the price participanst constraints.
+// Post: Returns the final array of objects of all the accepted applicaitons
+export function finalhackers(totalApplications, budget, totalHackers) {
+  // Step 1. Get the Local Hackers
+  const localApplications = totalApplications.filter(a => (a.airport === 'SEA'));
+  const localAcceptedApplications = localHackers(localApplications,
+                                      _.floor(LOCAL_HACKER_RATIO * totalHackers));
+  // Step 2. Get the Out of State Hackers
+  const remoteApplications = totalApplications.filter(a => (a.airport !== 'SEA'));
+  const remoteAcceptedHackers = remoteHackers(remoteApplications, budget,
+                                  _.floor((1 - LOCAL_HACKER_RATIO) * totalHackers));
+  // Step 3. Return the final hackers
+  return _.union(localAcceptedApplications, remoteAcceptedHackers);
+}
