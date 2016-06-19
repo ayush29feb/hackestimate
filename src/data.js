5af54d4a3c656f5675c8d3bf4a8f3370cfc6c16a
Ayush Saraf <ayush29f@uw.edu>
2016-06-19 06:57:18 -0700

    appending prices work

--- src/data.js
+++ src/data.js
@@ -1,3 +1,13 @@
-const data = require('../data/sample.csv');
+import _ from 'lodash';
+import { duplicate } from './util';
+import { priceFromAirport } from './prices';
 
-console.log(data);
+export function addPrices(data) {
+  const res = [];
+  _.forEach(data, (a) => {
+    const single = duplicate(a);
+    single.price = priceFromAirport(single.airport);
+    res.push(single);
+  });
+  return res;
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './styles/main.scss';
+import { addPrices } from './data';
 // import { finalhackers as logic } from './logic';
-import './data';
+
+const rawdata = require('../data/sample.csv');
+
+console.log(addPrices(rawdata));
